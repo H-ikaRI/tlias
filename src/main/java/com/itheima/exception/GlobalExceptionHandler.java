cd3e50d114f0af37c8a0c1347448b3b4bdf60e85
H_ikaRI <1426781969@qq.com>
2023-11-16 17:04:39 +0800

    完成了登录校验的功能

--- src/main/java/com/itheima/exception/GlobalExceptionHandler.java
+++ src/main/java/com/itheima/exception/GlobalExceptionHandler.java
@@ -18,8 +18,6 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(Exception.class)
     public Result ex(Exception ex){
         ex.printStackTrace();
-
-
         return Result.error("对不起，操作失败，请联系我");
     }
 }
